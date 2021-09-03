2753ace37a12ce755349cfa2ba249b0574610bc5
steveprabu <steverajprabu@gmail.com>
2021-09-03 10:35:33 +0800

    base script for extracting angular measurements of satellite passes

--- bin/orbit_extraction.sh
+++ bin/orbit_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --export=NONE
 #SBATCH -p workq
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=20
 #SBATCH --mem=124GB
 #SBATCH --tmp=440GB
@@ -34,9 +34,96 @@ PyLEO ./getTimeStepsFromMS.py --ms ${obsnum}.ms
 source tmp.txt
 
 echo "timeSteps found" ${TIMESTEPS} " and integration time " ${INTTIME}
+updatedTIMESTEPS=$(($TIMESTEPS-1)) ## cos indexes start from zero
+channels=768 ## hard coded 
+updatedCHANNELS=$(($channels-1))
 
-## do loop and process one-timestep at a time
+cp /home/sprabu/RFISeeker/RFISeeker /nvmetmp
 
+## loop and process one-timestep at a time
+for ((g=0;g<=${updatedTIMESTEPS};g++));
+do
+    echo "working on timeStep " ${g}
+    startt=`date +%s`
 
+    ## make images required for this time-step at every fine channel
+    i=$((g*1))
+    j=$((i+1))
+
+    ## spawn n number of jobs parallely
+    for f in `seq 0 ${updatedCHANNELS}`;
+    do
+        f1=$((f*1))
+        f2=$((f1+1))
+
+        while [[ $(jobs | wc -l) -ge 20 ]]
+        do
+            wait -n $(jobs -p)
+        done
+
+        ## unique temporary dump folder for every channel and time-step
+        mkdir temp_${g}_${f} 
+        name=`printf %04d $f`
+
+        wsclean -quiet -name ${obsnum}-2m-${i}-${name} -size 1400 1400\
+                        -temp-dir temp_${g}_${f} -abs-mem 5 -interval ${i} ${j}\
+                        -channel-range ${f1} ${f2} -weight natural -scale 5amin\
+                        -use-wgridder -no-dirty ${obsnum}.ms &
+
+    done
+
+    ## wait for all pids before continuing
+    b=0
+    for job in `jobs -p`
+    do
+        pids[${b}]=${job}
+        b=$((b+1))
+    done
+    for pid in ${pids[*]}; do
+        wait ${pid}
+    done
+
+    endt=`date +%s`
+    runtimet=$((endt-startt))
+    echo "the imaging run time ${runtimet}"
+
+    ## break out to next iteration if this is the first time-step
+    if [ ${g} -eq 0 ]; then
+        echo "this is the first time-step. continuing to next iteration.."
+        continue
+    fi
+    
+    startt=`date +%s`
+    ## run RFISeeker in parallel (for positive and negative source extraction)
+    python_rfi ./RFISeeker --obs ${obsnum} --freqChannels ${channels} --seedSigma 6\
+                --floodfillSigma 3 --timeStep $((g-1)) --prefix 6Sigma3Floodfill\
+                --imgSize 1400 --streak head --ext image &
+    PID1=$!
+
+    python_rfi ./RFISeeker --obs ${obsnum} --freqChannels ${channels} --seedSigma 6\
+                --floodfillSigma 3 --timeStep $((g-1)) --prefix 6Sigma3Floodfill\
+                --imgSize 1400 --streak Tail --ext image &
+    PID2=$!
+
+    wait ${PID1}    
+    wait ${PID2}
+
+    endt=`date +%s`
+    runtimet=$((endt-startt))
+    echo "the RFISeeker run time ${runtimet}"
+
+    ## clear files before proceeding to next time-step
+    rm -r temp_*
+    prevIndex=$((${g}-1))
+    rm ${obsnum}-2m-${prevIndex}-*.fits
+
+done
+
+## copy files back to 
+cp *RFI*.fits ${base}/processing/${obsnum}/
+
+end=`date +%s`
+runtime=$((end-start))
+echo "the job run time ${runtime}"
 
 }
