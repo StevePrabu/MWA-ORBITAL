e4809e01d2059f5ba61fef267dec9ae2b222abdc
steveprabu <steverajprabu@gmail.com>
2022-07-06 17:31:43 +0800

    base working version

--- bin/orbitDetermination.sh
+++ bin/orbitDetermination.sh
@@ -20,6 +20,7 @@ obsnum=OBSNUM
 base=BASE
 myPath=MYPATH
 norad=NORAD
+skip=SKIP
 
 cd ${base}/processing/${obsnum}
 
@@ -32,18 +33,40 @@ source tmp.txt
 echo "timeSteps found" ${TIMESTEPS} " and integration time " ${INTTIME}
 updatedTIMESTEPS=$(($TIMESTEPS-1)) ## cos indexes start from zero
 
-## extract angular position measurements of the pass
-cp ${myPath}/extractAngularMeasurements.py ${base}/processing/${obsnum}
-myPython3 ./extractAngularMeasurements.py --obs ${obsnum} --norad ${norad} --beamFile ${base}/models/beam.fits --user ${spaceTrackUser} --passwd ${spaceTrackPassword} --debug True
+## extract angular position measurements of the pass if skip=False
+if [ ${skip} = "true" ];
+then
+    echo "skipping extracAngularMeasurements.py"
+else
+    echo "not skipping extracAngularMeasurements.py"
+    cp ${myPath}/extractAngularMeasurements.py ${base}/processing/${obsnum}
+    myPython3 ./extractAngularMeasurements.py --obs ${obsnum} \
+            --norad ${norad} --beamFile ${base}/models/beam.fits\
+             --user ${spaceTrackUser} --passwd ${spaceTrackPassword} --debug True
+fi
+
 
 ## create initial guess for orbit determination
 cp ${myPath}/createConfig.py ${base}/processing/${obsnum}
-myPython3 ./createConfig.py --noradid ${norad} --wcsFile 6Sigma1FloodfillSigmaRFIBinaryMap-t0000.fits --debug True --user ${spaceTrackUser} --passwd ${spaceTrackPassword}
+myPython3 ./createConfig.py --noradid ${norad}\
+         --wcsFile 6Sigma1FloodfillSigmaRFIBinaryMap-t0000.fits --debug True\
+          --user ${spaceTrackUser} --passwd ${spaceTrackPassword}
 
 ## perform orbit determination
 cp ${myPath}/orbitFit.py ${base}/processing/${obsnum}
-myPython3 ./orbitFit.py --obs ${obsnum} --norad ${norad} --config auto_created_config${norad}.yaml --wcsFile 6Sigma1FloodfillSigmaRFIBinaryMap-t0000.fits --debug True
+myPython3 ./orbitFit.py --obs ${obsnum} --norad ${norad}\
+    --config auto_created_delta_0_config${norad}.yaml --niter 100 \
+    --wcsFile 6Sigma1FloodfillSigmaRFIBinaryMap-t0000.fits --debug True
 
+## make validation plots
+cp ${myPath}/validateAngularMeasurements.py ${base}/processing/${obsnum}
+myPython3 ./validateAngularMeasurements.py \
+    --file ${norad}_extracted_angular_measurements_${obsnum}.csv --norad ${norad}
+
+cp ${myPath}/validateOrbitalElements.py ${base}/processing/${obsnum}
+myPython3 ./validateOrbitalElements.py --wcsFile 6Sigma1FloodfillSigmaRFIBinaryMap-t0000.fits \
+    --norad ${norad} --user ${spaceTrackUser} --passwd ${spaceTrackPassword} \
+    --solutionFile 0delta${obsnum}n${norad}.txt
 
 end=`date +%s`
 runtime=$((end-start))
